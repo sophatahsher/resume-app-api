625ccd7d2d88db3b239f5c3b2e9dd3f44c4d3c8a
Bill <billboy6869@gmail.com>
2024-07-06 00:34:31 +0700

    fixed line containes whitespace in tests.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,13 +3,14 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module. """
+    """Test the calc module."""
+    
     def test_add_number(self):
         """Test add numbers together."""
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_number(self):
-        """Test subtracting numbers"""
-        res = calc.subtract(10,15)
-        self.assertEqual(res,-5)
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
